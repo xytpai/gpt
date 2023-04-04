21d875af4b9416f6985224547341aba2008b38ab
xytpai <xytpai@foxmail.com>
2023-04-04 18:43:17 +0800

    change vocab

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 read -p "Are you sure to clean? (Y/N):" para
 case $para in
     [yY])
-        rm -rf ./vocab.json
+        # rm -rf ./vocab.json
         rm -rf ./summary
         rm -rf ./checkpoints
         rm -rf __pycache__
